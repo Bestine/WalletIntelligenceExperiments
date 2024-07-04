91dc74f955f4ebd1cf3d52773aed6fb78306586a
Bestine <bestineokinda@gmail.com>
2024-07-04 12:14:35 +0300

    Added two plots to the dashboards

--- dashboards/dashboard_report.py
+++ dashboards/dashboard_report.py
@@ -13,13 +13,15 @@ df =  pd.read_csv("sampledata/sample1k_widata.csv")
 # Plot_1 -  a bar chart to count status codes 
 status_code_df = df["Status Code"].value_counts().reset_index()
 
-status_code_df.hvplot.bar(x="Status Code", y="count")
+plot_1 = status_code_df.hvplot.bar(x="Status Code", y="count")
 
-# Plot 2- 
+# Plot 2- Histogram of total balance
+plot_2 = df["Total Balance"].hvplot.hist()
 
 # Create the dashboard outlook 
 template = pn.template.FastListTemplate(
     title="WALLET QUICK ANALYSIS",
+    main = [plot_1, plot_2],
     accent_base_color="#88d8b0",
     header_background="#88d8b0",
 )
