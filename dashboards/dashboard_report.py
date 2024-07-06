b4043aa92bb258b2a1bcfb46b23ff1c76a7f53a2
Bestine <bestineokinda@gmail.com>
2024-07-06 11:12:54 +0300

    Cleaned the code then added a stlyed pie chart - Plot 1

--- dashboards/dashboard_report.py
+++ dashboards/dashboard_report.py
@@ -4,22 +4,33 @@ import hvplot.pandas  # noqa
 import hvplot.dask  # noqa
 from matplotlib import pyplot as plt
 import panel as pn
+import plotly.express as px
 
 hvplot.extension('matplotlib')
 
 # Load the required dataset
 df =  pd.read_csv("sampledata/sample1k_widata.csv")
 
-# Plot_1 -  a bar chart to count status codes 
-status_code_df = df["Status Code"].value_counts().reset_index()
+"""
+TO DO - PLOTTING
+- Create a pie/bar chart for bot shield. This will help the user identify how many bots and non bots ar
+- Find a good plot for token gating - 
+-  
+"""
 
-plot_1 = status_code_df.hvplot.bar(x="Status Code", y="count")
+# Plot_1 -  a bar chart to count Bots and non bots - BOT SHIELD
+bot_shield_df = df["Is Bot"].value_counts().reset_index()
 
-# Plot_2 - Histogram of total balance
-plot_2 = df["Total Balance"].hvplot.hist()
+plot_1 = px.pie(bot_shield_df, 
+                 values='count', names='Is Bot',
+                 title="Bot Shield - Composition of Bots and Non-bots")
 
-# Plot_3 - Scatter Plot of Total Balance vs Transaction Count:
-plot_3 = df.hvplot.scatter(x="Total Balance", y="Transaction Count")
+
+# # Plot_2 - Histogram of total balance
+# plot_2 = df["Total Balance"].hvplot.hist()
+
+# # Plot_3 - Scatter Plot of Total Balance vs Transaction Count:
+# plot_3 = df.hvplot.scatter(x="Total Balance", y="Transaction Count")
 
 # Plot_4 - Heatmap of Total Balance vs Transaction Count segmented by Status Code
 # PASS 
@@ -27,18 +38,10 @@ plot_3 = df.hvplot.scatter(x="Total Balance", y="Transaction Count")
 # Plot_5 - Bar Chart of Spend and Spend Games
 # PASS
 
-"""
-TO DO 
-- Create a pie/bar chart for bot shield. This will help the user identify how many bots and non bots ar
-- Find a good plot for token gating -  
-"""
-
 # Create the dashboard outlook 
 template = pn.template.FastListTemplate(
     title="WALLET QUICK ANALYSIS",
-    main = [plot_1, 
-            plot_2,
-            plot_3],
+    main = [plot_1,],
     accent_base_color="#88d8b0",
     header_background="#88d8b0",
 )
